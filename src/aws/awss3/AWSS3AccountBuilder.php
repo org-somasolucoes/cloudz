dec4b9b28308907d635b2bb79febebdcaaf1d726
Caio_Flor <caio.flor@somasolucoes.com>
2023-08-11 16:35:45 -0300

    chore: ajustar projeto de acordo com o retorno de Pull Request.

--- src/aws/awss3/AWSS3AccountBuilder.php
+++ src/aws/awss3/AWSS3AccountBuilder.php
@@ -6,7 +6,7 @@ class AWSS3AccountBuilder
 {
     private AWSS3Account $awsS3Account;
 
-    public function __construct($code)
+    public function __construct(int $code)
     {
         $this->awsS3Account = new AWSS3Account($code);
     }
